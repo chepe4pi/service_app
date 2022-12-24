dee213c5c93f7e045096781250caac5c3a20f4a5
Alexey Kurylev <chepe4pi@gmail.com>
2022-12-24 12:20:03 -0300

    Day 4 - 1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -13,7 +13,12 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price -
+                instance.service.full_price * (instance.plan.discount_percent / 100))
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
